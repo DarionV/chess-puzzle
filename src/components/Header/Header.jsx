c0cffdcb4c97ec488d45ed57f283413da8aa85ed
Darion Valdez <darionvldz@gmail.com>
2024-11-02 16:43:53 +0100

    add basic styling to Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import styles from "./header.module.css";
 
 const Header = () => {
   const location = useLocation();
   const isOnTutorialPage = location.pathname === "/how-to-play";
 
   return (
-    <header>
+    <header className={styles.header}>
       <div className="theme-switcher"></div>
-      <nav>
+      <nav className={styles.navigation}>
         <a
           href="https://www.etsy.com/se-en/listing/1718348748/pawn-queen-puzzle-get-the-pawn-to-the?click_key=ae933615e6048cb3c2e870882acbd05a13ca25ef%3A1718348748&click_sum=7a8479e2&external=1&rec_type=ss&ref=landingpage_similar_listing_top-7&sts=1"
           target="blank"
