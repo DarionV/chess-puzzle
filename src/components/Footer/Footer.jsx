f4d0a99d3298e352211b06deae2d4e2795f461d5
Darion Valdez <darionvldz@gmail.com>
2024-11-02 20:22:34 +0100

    implement basic theme styles for footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import style from "./Footer.module.css";
+import lightStyle from "./FooterLight.module.css";
+import darkStyle from "./FooterDark.module.css";
+import { useTheme } from "../../context/ThemeContext";
 
 const Footer = () => {
+  const theme = useTheme();
+  const themeStyling = theme === "light" ? lightStyle : darkStyle;
+
   return (
-    <footer className={style.footer}>
+    <footer className={`${style.footer} ${themeStyling.footer}`}>
       Original puzzle by{" "}
       <a href="#" target="blank">
         Sherzod Khaydarbekov
