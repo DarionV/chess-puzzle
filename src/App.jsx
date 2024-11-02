a5972e1e3fa94ccfdc19575da95c9a458b506471
Darion Valdez <darionvldz@gmail.com>
2024-11-02 16:30:41 +0100

    implement navigation with react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import { Link, Outlet, useLocation } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const location = useLocation();
+
+  const isOnTutorialPage = location.pathname === "/how-to-play";
+
+  return (
+    <>
+      <header>
+        <div className="theme-switcher"></div>
+        <a
+          href="https://www.etsy.com/se-en/listing/1718348748/pawn-queen-puzzle-get-the-pawn-to-the?click_key=ae933615e6048cb3c2e870882acbd05a13ca25ef%3A1718348748&click_sum=7a8479e2&external=1&rec_type=ss&ref=landingpage_similar_listing_top-7&sts=1"
+          target="blank"
+        >
+          GET THE GAME
+        </a>
+        <Link to={isOnTutorialPage ? "/" : "/how-to-play"}>
+          {isOnTutorialPage ? "BACK TO GAME" : "HOW TO PLAY"}
+        </Link>
+      </header>
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Game from "./pages/Game";
+import Tutorial from "./pages/Tutorial";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        // path: "/play",
+        element: <Game />,
+      },
+      {
+        path: "/how-to-play",
+        element: <Tutorial />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
