bbe1a1fdedf76583354d3eb884bebf13197c268a
Darion Valdez <darionvldz@gmail.com>
2024-11-03 20:20:12 +0100

    implement tiling pattern and red tile

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,27 +1,34 @@
 import React from "react";
 import Tile from "../Tile";
-import tileWhite from "../../assets/images/tile-white-2.png";
+import tileWhite from "../../assets/images/tile-white.png";
+import tileBlack from "../../assets/images/tile-black.png";
+import tileRed from "../../assets/images/tile-red.png";
 
 const Board = () => {
   const board = [
     ["empty", "empty", "empty", "empty"],
     ["empty", "empty", "empty", null],
     ["empty", "empty", "empty", null],
-    ["empty", "empty", "empty", "empty"],
+    ["empty", "empty", "empty", "empty red"],
   ];
 
   let currentRow = 0;
   let tiles = [];
+  let color;
 
-  const TILE_HEIGHT_IN_PX = 160;
+  const TILE_HEIGHT_IN_PX = 124;
   const TILE_WIDTH_IN_PX = 160;
 
   board.forEach((row) => {
+    currentRow % 2 === 0 ? (color = tileWhite) : (color = tileBlack);
+
     row.forEach((tile, index) => {
-      if (tile) {
-        const tileObject = { xpos: index, ypos: currentRow };
-        tiles.push(tileObject);
-      }
+      if (!tile) return;
+      if (tile.includes("red")) color = tileRed;
+      const tileObject = { xpos: index, ypos: currentRow, color: color };
+      tiles.push(tileObject);
+
+      color === tileBlack ? (color = tileWhite) : (color = tileBlack);
     });
     currentRow++;
   });
@@ -32,15 +39,15 @@ const Board = () => {
     <div style={{ position: "relative" }}>
       {tiles.map((tile, index) => (
         <img
-          src={tileWhite}
+          src={tile.color}
           width={`${TILE_WIDTH_IN_PX}px`}
           height={`${TILE_HEIGHT_IN_PX}px`}
           key={index}
           style={{
             position: "absolute",
             top: `${
-              tile.xpos * 0.29 * TILE_HEIGHT_IN_PX +
-              tile.ypos * 0.29 * TILE_HEIGHT_IN_PX
+              tile.xpos * 0.37 * TILE_HEIGHT_IN_PX +
+              tile.ypos * 0.37 * TILE_HEIGHT_IN_PX
             }px`,
             left: `${
               tile.xpos * 0.5 * TILE_WIDTH_IN_PX +
