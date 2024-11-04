aa715187818519583471f353cb3f0a9ba2069533
Darion Valdez <darionvldz@gmail.com>
2024-11-04 21:29:20 +0100

    adjust touchTargets sizes

--- src/components/TouchTarget/TouchTarget.jsx
+++ src/components/TouchTarget/TouchTarget.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
 import style from "./TouchTarget.module.css";
 
-const TouchTarget = ({ piece, size, yPos, xPos, zIndex }) => {
+const TouchTarget = ({ size, yPos, xPos, zIndex }) => {
   const topOffset = 87; //To center piece in tile properly
-  const leftOffset = 20;
-  const TOP_MARGIN_IN_PX = -size / topOffset + 80;
+  const leftOffset = size * 0.15;
+  //   const TOP_MARGIN_IN_PX = -size / topOffset + 80;
 
   return (
     <button
       className={style.touchTarget}
       style={{
+        width: size * 0.7,
+        height: size * 0.7,
         position: "absolute",
         top: `${xPos * 0.29 * size + yPos * 0.29 * size + topOffset}px`,
         left: `${
