c12e3408e4de5a0c1b3b4e48f48d48d7584fe89d
Darion Valdez <darionvldz@gmail.com>
2024-11-06 13:08:47 +0100

    remove unused files

--- src/utils/getColor.jsx
+++
@@ -1,6 +0,0 @@
-import tileRed from "../assets/images/tile-red.png";
-
-export const getColor = (color, tileWhite, tileBlack, tile = "") => {
-  color === tileBlack ? (color = tileWhite) : (color = tileBlack);
-  return tile.includes("red") ? tileRed : color;
-};
