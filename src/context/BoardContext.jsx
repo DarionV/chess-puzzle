aa4ceed46a26af776ab9d17f2051031dfa0d003e
Darion Valdez <darionvldz@gmail.com>
2024-11-07 16:17:26 +0100

    remove comments

--- src/context/BoardContext.jsx
+++ src/context/BoardContext.jsx
@@ -1,17 +1,9 @@
 import React, { createContext, useState } from "react";
+import puzzles from "../puzzlesDb.js";
 
 export const BoardContext = createContext();
 
 export const BoardProvider = ({ children }) => {
-  // P: Pawn
-  // N: Knight
-  // B: Bishop
-  // R: Rook
-  // Q: Queen
-  // K: King - not implemented
-  // G: Goal - Red goal square.
-  // H: Hero - Piece that needs to reach the goal.
-  // Always start with Piece, eg. PH.
   const [board, setBoard] = useState([
     ["B", "N", "R", "PH"],
     ["B", "N", "R", null],
