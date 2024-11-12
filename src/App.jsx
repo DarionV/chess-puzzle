dbd37a8b7e511caec88c87c6e7991ba49fb06e4d
Darion Valdez <darionvldz@gmail.com>
2024-11-12 15:48:33 +0100

    remove unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
-import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import { useTheme } from "./context/ThemeContext";
 
 function App() {
-  const theme = useTheme();
-  const themeStyling = theme === "light" ? "light" : "dark";
   return (
     <>
-      <Header />
-      <main className={themeStyling}>
+      <main>
         <Outlet />
       </main>
       <Footer />
