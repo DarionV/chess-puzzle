a4592cbebf160eb1e5bbe80cb4c3580d988f1988
Darion Valdez <darionvldz@gmail.com>
2024-11-15 19:19:42 +0100

    add min size to board

--- src/hooks/useTileSize.jsx
+++ src/hooks/useTileSize.jsx
@@ -3,10 +3,13 @@ import React, { useEffect, useState } from "react";
 const useTileSize = () => {
   const [tileSize, setTileSize] = useState(200);
   const MAX_SIZE = 220;
+  const MIN_SIZE = 100;
 
   const handleResize = () => {
     const newSize = window.innerWidth / 5.5;
-    setTileSize((prevSize) => (newSize > MAX_SIZE ? MAX_SIZE : newSize));
+    setTileSize((prevSize) =>
+      newSize > MAX_SIZE ? MAX_SIZE : newSize < MIN_SIZE ? MIN_SIZE : newSize
+    );
   };
 
   useEffect(() => {
