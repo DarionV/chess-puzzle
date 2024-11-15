23af340081dc92fd687bf17a5e1504d58f446822
Darion Valdez <darionvldz@gmail.com>
2024-11-15 14:14:39 +0100

    moved component

---
+++ src/components/AnimatedHeading/AnimatedHeading.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import style from "./AnimatedHeading.module.css";
+
+const AnimatedHeading = ({ heading }) => {
+  const [oldHeading, setOldHeading] = useState(heading);
+  const [animationClass, setAnimationClass] = useState(style.fadeIn);
+
+  useEffect(() => {
+    if (heading !== oldHeading) {
+      setAnimationClass(style.fadeOut);
+      setTimeout(() => {
+        setOldHeading(heading);
+        setAnimationClass(style.fadeIn);
+      }, 300);
+    }
+  }, [heading]);
+
+  return (
+    <div className={style.container}>
+      <h1 className={`${style.layer0} ${animationClass}`}>{oldHeading}</h1>
+      <h2 className={`${style.layer1} ${animationClass}`}>{oldHeading}</h2>
+      <h2 className={`${style.layer2} ${animationClass}`}>{oldHeading}</h2>
+      <h2 className={`${style.layer3} ${animationClass}`}>{oldHeading}</h2>
+    </div>
+  );
+};
+
+export default AnimatedHeading;
