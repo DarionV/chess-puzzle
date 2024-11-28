af1a0277a572c2c6fa40b7496d715a7b2c83b4d1
Darion Valdez <darionvldz@gmail.com>
2024-11-28 19:42:29 +0100

    add basic styling to 404 page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import king404 from "../assets/images/king-404.png";
 import { Link } from "react-router-dom";
+import { Center } from "@mantine/core";
+import { Stack } from "@mantine/core";
 
 const Error = () => {
   return (
-    <div>
-      <h1>Oh no!</h1>
-      <p>We couldn't find the puzzle you were looking for.</p>
-      <Link to={"/"}>Back to home</Link>
-      <img src={king404} alt="404" width={200} />
-    </div>
+    <Center>
+      <Stack align="center">
+        <h1>Oh no!</h1>
+        <p>We couldn't find the puzzle you were looking for.</p>
+        <img src={king404} alt="404" width={200} />
+        <Link to={"/"}>Back to home</Link>
+      </Stack>
+    </Center>
   );
 };
 
