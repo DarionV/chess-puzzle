42b9ea2cbcd86434ed7f36ad3d1d8775e9c42df4
Darion Valdez <darionvldz@gmail.com>
2024-11-29 16:34:24 +0100

    implement theme toggle

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import styles from "./Header.module.css";
 import BoardContext from "../../context/BoardContext";
 import {
@@ -6,27 +6,39 @@ import {
   useComputedColorScheme,
   useMantineColorScheme,
 } from "@mantine/core";
+import { IconMoonFilled, IconSunFilled } from "@tabler/icons-react";
 
 export default function Header({ toggleAbout }) {
-  const { getBuyLink, resetBoard } = useContext(BoardContext);
+  const { resetBoard } = useContext(BoardContext);
   const { colorScheme, setColorScheme } = useMantineColorScheme({
     keepTransitions: true,
   });
 
-  const computedColorScheme = useComputedColorScheme("dark");
+  const computedColorScheme = useComputedColorScheme("light");
 
   const toggleColorScheme = () => {
-    setColorScheme(colorScheme === "light" ? "dark" : "light");
+    setColorScheme(computedColorScheme === "light" ? "dark" : "light");
   };
+
+  const [isDark, setIsDark] = useState(colorScheme);
+
+  useEffect(() => {
+    setIsDark(colorScheme === "light");
+  }, [colorScheme]);
+
+  const moonIcon = <IconMoonFilled style={{ width: 20, height: 20 }} />;
+  const sunIcon = <IconSunFilled style={{ width: 20, height: 20 }} />;
 
   return (
     <header className={styles.header}>
       <button onClick={resetBoard}>RESET</button>
       <button onClick={toggleAbout}>ABOUT</button>
       <Switch
-        onLabel="Light"
-        offLabel="Dark"
+        color="gray"
+        onLabel={moonIcon}
+        offLabel={sunIcon}
         size="lg"
+        checked={isDark}
         onClick={toggleColorScheme}
       ></Switch>
     </header>
