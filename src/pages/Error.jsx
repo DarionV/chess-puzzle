f0f23e4a54d5f4d8c538172971f3f01d11d83392
Darion Valdez <darionvldz@gmail.com>
2024-11-29 18:09:07 +0100

    styling error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import king404 from "../assets/images/king-404.png";
-import { Link } from "react-router-dom";
 import { Center } from "@mantine/core";
 import { Stack } from "@mantine/core";
+import StandardButton from "../components/StandardButton/StandardButton";
+import { useNavigate } from "react-router";
 
 const Error = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/");
+  };
+
   return (
     <Center>
       <Stack align="center">
         <h1>Oh no!</h1>
         <p>We couldn't find the puzzle you were looking for.</p>
-        <img src={king404} alt="404" width={200} />
-        <Link to={"/"}>Back to home</Link>
+        <img src={king404} alt="404" width={400} />
+        <StandardButton handleClick={handleClick}>Back to home</StandardButton>
       </Stack>
     </Center>
   );
