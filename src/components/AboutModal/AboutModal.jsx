9b725b7439a74795cab119ed2cf71dc04ced0aa4
Darion Valdez <darionvldz@gmail.com>
2024-11-29 16:53:09 +0100

    restyle about modal

--- src/components/AboutModal/AboutModal.jsx
+++ src/components/AboutModal/AboutModal.jsx
@@ -27,33 +27,31 @@ const AboutModal = ({ opened, toggleAbout }) => {
         },
       }}
     >
+      <h3>Developer</h3>
       <a
         href="https://www.linkedin.com/in/darion-valdez-a63629176/"
         target="blank"
       >
-        Developed by DarionV
+        DarionV
       </a>
+      <h3>Puzzle designs</h3>
       <a href="https://uz.linkedin.com/in/khaydarbekoff" target="blank">
-        Puzzle designs by Sherzod Khaydarbekov
+        Sherzod Khaydarbekov
       </a>
+      <a
+        href="https://www.linkedin.com/in/darion-valdez-a63629176/"
+        target="blank"
+      >
+        DarionV
+      </a>
+      <h3>3D Assets</h3>
       <a
         href="https://www.thingiverse.com/thing:6054837"
         target="blank"
         title="3D-assets"
       >
-        3D-assets by Stanleyerland @ Thingiverse
+        Stanleyerland @ Thingiverse
       </a>
-      <a
-        href="https://www.flaticon.com/free-icons/knight"
-        title="chess icons"
-        target="blank"
-      >
-        Chess icons created by rizal2109 - Flaticon
-      </a>
-      {/* <p>
-        We use affiliate links, which help us earn commissions from purchases
-        made through this site.
-      </p> */}
     </Modal>
   );
 };
